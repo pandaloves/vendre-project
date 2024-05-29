79fee92c7774202e7f02f5d40b21f78d110a13b4
meiyi <meiyinghu18@gmail.com>
2024-05-30 00:37:28 +0200

    new

--- src/main.js
+++ src/main.js
@@ -8,9 +8,8 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
-// Importing components for routes
-import EmployeesTabs from "./views/EmployeesTabs.vue";
-import EmployeeCard from "./components/EmployeeCard.vue";
+// Importing components
+import EmployeesTabs from "./pages/EmployeesTabs.vue";
 import AppFooter from "./components/AppFooter.vue";
 import AppNavbar from "./components/AppNavbar.vue";
 
@@ -21,7 +20,6 @@ const vuetify = createVuetify({
 
 const routes = [
   { path: "/", component: EmployeesTabs },
-  { path: "/employee/:id", component: EmployeeCard },
   { path: "/appnavbar", component: AppNavbar },
   { path: "/appfooter", component: AppFooter },
 ];
